36cdffe3a36844a6188efd91bcecd8be4e717b4e
ergin demirel <ergdemirel@hotmail.com>
2023-04-06 00:20:31 -0700

    Some message here

--- Python/data_mining.py
+++ Python/data_mining.py
@@ -1,3 +1,5 @@
+from math import sqrt
+
 users = {"Angelica": {"Blues Traveler": 3.5, "Broken Bells": 2.0,
 "Norah Jones": 4.5, "Phoenix": 5.0,"Slightly Stoopid": 1.5,
 "The Strokes": 2.5, "Vampire Weekend": 2.0},
@@ -37,22 +39,25 @@ def manhattan(rating1, rating2):
 #print(manhattan(users['Hailey'], users['Veronica']))
 
 
-def computeNearestNeighbor(username, users):
+def computeNearestNeighbor(username, users, algorithm):
     distances = []
     for user in users:
         if user != username:
-            distance = manhattan(users[user], users[username])
+            if algorithm==1:
+                distance = manhattan(users[user], users[username])
+            else:
+                distance = minkowski(users[user], users[username],2)
             distances.append((distance, user))
         # sort based on distance -- closest first
         distances.sort()
     return distances
 
-print(computeNearestNeighbor("Hailey", users))
+#print(computeNearestNeighbor("Hailey", users))
 
 
 def recommend(username, users):
 # first find nearest neighbor
-    nearest = computeNearestNeighbor(username, users)[0][1]
+    nearest = computeNearestNeighbor(username, users,1)[0][1]
     recommendations = []
     # now find bands neighbor rated that user didn't
     neighborRatings = users[nearest]
@@ -65,3 +70,49 @@ def recommend(username, users):
 
 
 print(recommend('Hailey', users))
+
+
+def minkowski(rating1, rating2, r):
+    distance = 0
+    commonRatings = False
+    for key in rating1:
+        if key in rating2:
+            distance += pow(abs(rating1[key] - rating2[key]), r)
+            commonRatings = True
+        if commonRatings:
+            return pow(distance, 1/r)
+        else:
+            return 0 #Indicates no ratings in common
+        
+
+print(computeNearestNeighbor("Hailey", users,1))
+
+def pearson(rating1, rating2):
+    sum_xy = 0
+    sum_x = 0
+    sum_y = 0
+    sum_x2 = 0
+    sum_y2 = 0
+    n = 0
+    for key in rating1:
+        if key in rating2:
+            n += 1
+            x = rating1[key]
+            y = rating2[key]
+            sum_xy += x * y
+            sum_x += x
+            sum_y += y
+            sum_x2 += x**2
+            sum_y2 += y**2
+    # if no ratings in common return 0
+    if n == 0:
+        return 0
+    # now compute denominator
+    denominator = sqrt(sum_x2 - (sum_x**2) / n) * sqrt(sum_y2 - (sum_y**2) / n)
+    if denominator == 0:
+        return 0
+    else:
+        return (sum_xy - (sum_x * sum_y) / n) / denominator
+    
+    
+print(pearson(users['Angelica'], users['Bill']))
